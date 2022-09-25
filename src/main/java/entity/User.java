774e16114438bd35463042ed1abd05b77cc35521
un-go <margofani.un.go@gmail.com>
2022-09-25 10:11:00 +0300

    add application to github repository

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,7 @@
+package entity;
+
+public class User {
+    private static long userId;
+    private static String firstName;
+    private static String lastName;
+}
---
+++ src/main/java/servlets/UserServlet.java
@@ -0,0 +1,8 @@
+package servlets;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+}
